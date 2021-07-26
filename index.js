9bf379aac292625b83e3866c64d3a756319c6f14
rainverted <MY_NAME@example.com>
2021-07-26 20:08:11 +0300

    File system: create

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ _data.read('users', 'petras', (err, data) => {
     console.log(obj.name);
     console.log(obj.age);
     console.log(obj.car);
+    console.log(obj.married);
 })
 
 // _data.read('users', 'maryte', (err, data) => {
@@ -21,3 +22,23 @@ _data.read('users', 'petras', (err, data) => {
 
 //     console.log(data);
 // })
+
+const pazymiai = [10, 2, 8, 4, 6];
+_data.create('marks', 'onute', pazymiai, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+
+    console.log('Failas sekmingai sukurtas!');
+
+    _data.read('marks', 'onute', (err, data) => {
+        if (err || !data) {
+            console.log('Nepavyko perskaityti Onutes failo...');
+            return false;
+        }
+
+        const obj = JSON.parse(data);
+        console.log(obj);
+    })
+});
