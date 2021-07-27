6f3143fa56a972b3b89ed1331dfd3a4624d926ee
rainverted <MY_NAME@example.com>
2021-07-27 18:33:13 +0300

    file system: update

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 const _data = require('./lib/data');
 
-_data.read('users', 'petras', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
-
-    const obj = JSON.parse(data);
-    console.log(obj);
-    console.log(obj.name);
-    console.log(obj.age);
-    console.log(obj.car);
-    console.log(obj.married);
-})
-
-// _data.read('users', 'maryte', (err, data) => {
+// _data.read('users', 'petras', (err, data) => {
 //     if (err || !data) {
 //         console.log('Nepavyko perskaityti failo...');
 //         return false;
 //     }
 
-//     console.log(data);
+//     const obj = JSON.parse(data);
+//     console.log(obj);
 // })
 
-const pazymiai = [10, 2, 8, 4, 6];
-_data.create('marks', 'onute', pazymiai, (err) => {
+
+// const pazymiai = [10, 2, 8, 4, 6];
+// _data.create('marks', 'kazys', pazymiai, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
+
+//     console.log('Failas sekmingai sukurtas!');
+
+//     _data.read('marks', 'kazys', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Kazio failo...');
+//             return false;
+//         }
+
+//         const obj = JSON.parse(data);
+//         console.log(obj);
+//     })
+// });
+
+const petras = {
+    name: 'Petras',
+    age: 99,
+    car: 'Audi',
+
+}
+_data.update('users', 'petras', petras, (err) => {
     if (err) {
         console.log(err);
         return false;
     }
 
-    console.log('Failas sekmingai sukurtas!');
+    console.log('Petro duomenys atnaujinti sekmingai.');
 
-    _data.read('marks', 'onute', (err, data) => {
+    _data.read('users', 'petras', (err, data) => {
         if (err || !data) {
-            console.log('Nepavyko perskaityti Onutes failo...');
+            console.log('Nepavyko perskaityti Petro failo...');
             return false;
         }
 
