5bb016fa261a1e77d503687f63ec460fdfef3671
rainverted <MY_NAME@example.com>
2021-07-29 18:07:36 +0300

    File system: delete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const _data = require('./lib/data');
 
 // _data.read('users', 'petras', (err, data) => {
@@ -31,27 +32,36 @@ const _data = require('./lib/data');
 //     })
 // });
 
-const petras = {
-    name: 'Petras',
-    age: 99,
-    car: 'Audi',
+// const petras = {
+//     name: 'Petras',
+//     age: 99,
+//     car: 'Audi',
+//     color: 'red'
+// }
+// _data.update('users', 'petras', petras, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
 
-}
-_data.update('users', 'petras', petras, (err) => {
+//     console.log('Petro duomenys atnaujinti sekmingai.');
+
+//     _data.read('users', 'petras', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Petro failo...');
+//             return false;
+//         }
+
+//         const obj = JSON.parse(data);
+//         console.log(obj);
+//     })
+// });
+
+_data.delete('users', 'maryte', (err) => {
     if (err) {
         console.log(err);
         return false;
     }
 
-    console.log('Petro duomenys atnaujinti sekmingai.');
-
-    _data.read('users', 'petras', (err, data) => {
-        if (err || !data) {
-            console.log('Nepavyko perskaityti Petro failo...');
-            return false;
-        }
-
-        const obj = JSON.parse(data);
-        console.log(obj);
-    })
-});
+    console.log('Failas sekmingai istrintas.');
+})
